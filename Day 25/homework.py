2b0f1b1d24aae9d918a8625cfbd341c256811ec9
nikqime <155996969+nikqime@users.noreply.github.com>
2024-07-26 17:41:25 +0400

    commit

---
+++ Day 25/homework.py
@@ -0,0 +1,17 @@
+my_list = []
+
+# მნიშვნელობის დამატება სიის ბოლოში
+my_list.append('one')
+print( my_list)
+
+# ნებისმიერი მნიშვნელობის ამოშლა
+my_list.remove('1')
+print( my_list)
+
+# სიის ზომის გაგება
+list_length = len(my_list)
+print(list_length)
+
+# ახალი მნიშვნელობის დამატება მეორე ინდექსზე
+my_list.insert(1, 2)
+print(my_list)
