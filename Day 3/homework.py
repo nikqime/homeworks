cea256cdbb15bc8bd891e0af8e4af56138dcc9c0
nikqime <155996969+nikqime@users.noreply.github.com>
2024-07-26 14:30:28 +0400

    commit

---
+++ Day 3/homework.py
@@ -0,0 +1,4 @@
+father=40
+mother=40
+print(father+20)
+print(mother+20)
---
+++ Day 6/homework.py
@@ -0,0 +1,5 @@
+question=input("How much salary do u have?: ")
+if question>10000:
+    print("that's nice")
+elif question<10000:
+    print("bad")
